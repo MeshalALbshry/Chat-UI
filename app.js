45e21851c8b80f810f44d23cb78a485f61fab2db
MESHAL <meshal.albshry@gmail.com>
2018-03-14 14:43:42 +0300

    Add scroll down

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){
-    $("#btn-send").click(function(){
-        var input = $("#input-massage").val()
-        $("#msg").append('<li class="me">'+input+'</li>');
-        $("#msg").append('<li class="him">Hello</li>');
+$(document).ready(function () {
+    $("#btn-send").click(function () {
+        var input = $("#input-message").val()
+        $("#input-message").val('')
+        $("#msg").append('<li class="message" id="me">' + input + '</li>');
+        //Scroll down
+        $('#messages').animate({
+            scrollTop: $('#messages').get(0).scrollHeight}, 0);
     })
+
+    var count = 0;
+    setInterval(function(){
+        $("#msg").append('<li class="message" id="him">' + 'Hello ' + count++ +'</li>');
+        //Scroll down
+        $('#messages').animate({
+            scrollTop: $('#messages').get(0).scrollHeight}, 0);
+    },7000)
 });
 
 
