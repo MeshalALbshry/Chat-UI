25f0414e24d3211ab6c880863f81e96ea4fc9cdc
MESHAL <meshal.albshry@gmail.com>
2018-03-15 10:40:16 +0300

    Add Brodcast design

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 $(document).ready(function () {
+
     $("#btn-send").click(function () {
         var input = $("#input-message").val()
         $("#input-message").val('')
         $("#msg").append('<li class="message" id="me">' + input + '</li>');
         //Scroll down
-        $('#messages').animate({
-            scrollTop: $('#messages').get(0).scrollHeight}, 0);
+        scrollDown()
     })
-
+    var bordcast = "التعميم هو قانون منطقي يحلل تفسير الشامل بملاحظة المحدود. والفكرة تطرح ان في نطاق ما، او مجموعةعناصر لها خصائص مشتركة، فانه يمكن اعتماد نتيجة عامل واحد كنتيجة شامله للك    عندما يقوم العقل بالتعميم، فإنه يستخراج جوهر مفهوم على أساس تحليله من أوجه التشابه من العديد من الكائنات المنفصلة. فالتبسيط الناتج يسمح بالفكير بمستويات أعلى";
     var count = 0;
     setInterval(function(){
         $("#msg").append('<li class="message" id="him">' + 'Hello ' + count++ +'</li>');
         //Scroll down
+        scrollDown()
+    },7000)
+
+    setInterval(function(){
+        $("#msg").append('<li class="message" id="brodcast">' + bordcast +'</li>');
+        //Scroll down
+        scrollDown()
+    },17000)
+
+
+    function scrollDown(){
         $('#messages').animate({
             scrollTop: $('#messages').get(0).scrollHeight}, 0);
-    },7000)
+    }
+
+
 });
 
 
